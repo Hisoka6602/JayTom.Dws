94282f1243c32f39883584186d98d9079257f3df
Hisoka <770512100@qq.com>
2023-06-23 18:46:15 +0800

    20230623

---
+++ JayTom.Dws.Domain/Repository/Logs/IInstructionLogRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JayTom.Dws.Data.LocalLog;
+using JayTom.Dws.Data.LocalData;
+using System.Collections.Generic;
+
+namespace JayTom.Dws.Domain.Repository.Logs {
+
+    public interface IInstructionLogRepository : IRepository<InstructionLogInfoModel> {
+    }
+}
