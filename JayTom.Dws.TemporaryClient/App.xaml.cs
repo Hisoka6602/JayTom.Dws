bd4c466a29eca396617ed7fd11331d1cd9520c71
Hisoka <770512100@qq.com>
2023-06-27 18:30:57 +0800

    20230627

--- JayTom.Dws.TemporaryClient/App.xaml.cs
+++ JayTom.Dws.TemporaryClient/App.xaml.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+using Prism.Ioc;
+using Prism.Mvvm;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using Prism.DryIoc;
 using System.Windows;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Collections.Generic;
+using JayTom.Dws.TemporaryClient.ViewModels;
 
 namespace JayTom.Dws.TemporaryClient {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App : PrismApplication {
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void ConfigureViewModelLocator() {
+            base.ConfigureViewModelLocator();
+            //绑定页面
+            ViewModelLocationProvider.Register<MainWindow, MainWindowViewModel>();
+            //LoadingDialogViewModel
+        }
+
+        protected override void OnStartup(StartupEventArgs e) {
+            //启动函数
+            this.DispatcherUnhandledException += delegate (object sender, DispatcherUnhandledExceptionEventArgs args) {
+                //异常触发
+            };
+            AppDomain.CurrentDomain.UnhandledException += delegate (object sender, UnhandledExceptionEventArgs args) {
+                //异常触发
+            };
+            base.OnStartup(e);
+        }
     }
 }
---
+++ JayTom.Dws.TemporaryClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Prism.Mvvm;
+using Prism.Regions;
+using System.Windows;
+using Prism.Commands;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+
+namespace JayTom.Dws.TemporaryClient.ViewModels {
+
+    public class MainWindowViewModel : BindableBase {
+        private readonly IRegionManager _regionManager;
+        private double _uniformCornerRadius = 10;
+        private string _maxBtnIcon = "\xe600";
+        private string _maxBtnToolTip = "最大化";
+        private SnackbarMessageQueue _mainMessageQueue = new(TimeSpan.FromSeconds(2));
+
+        public MainWindowViewModel(IRegionManager regionManager) {
+            _regionManager = regionManager;
+        }
+
+        public double UniformCornerRadius {
+            get => _uniformCornerRadius;
+            set => SetProperty(ref _uniformCornerRadius, value);
+        }
+
+        /// <summary>
+        /// 最大化按钮图标
+        /// </summary>
+        public string MaxBtnIcon {
+            get => _maxBtnIcon;
+            set => SetProperty(ref _maxBtnIcon, value);
+        }
+
+        /// <summary>
+        /// 最大化按钮提示内容
+        /// </summary>
+        public string MaxBtnToolTip {
+            get => _maxBtnToolTip;
+            set => SetProperty(ref _maxBtnToolTip, value);
+        }
+
+        /// <summary>
+        /// 提示内容
+        /// </summary>
+        public SnackbarMessageQueue MainMessageQueue {
+            get => _mainMessageQueue;
+            set => SetProperty(ref _mainMessageQueue, value);
+        }
+
+        public ICommand LoadedCommand {
+            get => new DelegateCommand<object>(LoadedDelegate);
+        }
+
+        public ICommand MinWinCommand {
+            get => new DelegateCommand<object>(MinWinDelegate);
+        }
+
+        public ICommand MaxWinCommand {
+            get => new DelegateCommand<object>(MaxWinDelegate);
+        }
+
+        public ICommand CloseWinCommand {
+            get => new DelegateCommand<object>(CloseWinDelegate);
+        }
+
+        public ICommand SizeChangedCommand {
+            get => new DelegateCommand<object>(SizeChangeDelegate);
+        }
+
+        public ICommand PageSwitchingCommand {
+            get => new DelegateCommand<object>(PageSwitchingDelegate);
+        }
+
+        /// <summary>
+        /// 页面切换
+        /// </summary>
+        /// <param name="obj"></param>
+        private async void PageSwitchingDelegate(object obj) {
+            await System.Windows.Application.Current.Dispatcher.InvokeAsync(() => {
+                _regionManager.Regions["ContentRegion"].RequestNavigate(obj.ToString());
+            });
+        }
+
+        private void SizeChangeDelegate(object obj) {
+            if (obj is Window window) {
+                window.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+                window.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+                if (window.WindowState == WindowState.Maximized ||
+                    (window.Height >= SystemParameters.MaximizedPrimaryScreenHeight &&
+                     window.Width >= SystemParameters.MaximizedPrimaryScreenWidth)) {
+                    //直角
+                    UniformCornerRadius = 0;
+                }
+                else {
+                    UniformCornerRadius = 10;
+                    //圆角
+                }
+                if (window.WindowState == WindowState.Maximized) {
+                    MaxBtnIcon = "\xe72c";
+                    MaxBtnToolTip = "还原";
+                }
+                else {
+                    MaxBtnIcon = "\xe600";
+                    MaxBtnToolTip = "最大化";
+                }
+            }
+        }
+
+        private void CloseWinDelegate(object obj) {
+            System.Windows.Application.Current.Shutdown();//关闭
+        }
+
+        private void MaxWinDelegate(object obj) {
+            if (obj is Window window) {
+                if (window.WindowState == WindowState.Maximized) {
+                    window.WindowState = WindowState.Normal;
+                    return;
+                }
+                window.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private async void LoadedDelegate(object obj) {
+            /*await System.Windows.Application.Current.Dispatcher.InvokeAsync(() => {
+                _regionManager.Regions["ContentRegion"].RequestNavigate("HomeView");
+            });*/
+        }
+
+        private void MinWinDelegate(object obj) {
+            if (obj is Window window) {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+    }
+}
