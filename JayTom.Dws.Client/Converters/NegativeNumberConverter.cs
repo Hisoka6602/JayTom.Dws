fef581149a3477bebe2b66879920d2ad494d25ba
Hisoka <770512100@qq.com>
2023-08-31 21:49:52 +0800

    20230831

---
+++ JayTom.Dws.Client/Converters/NegativeNumberConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace JayTom.Dws.Client.Converters {
+
+    public class NegativeNumberConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var number = (double)value;
+
+            // 自定义处理负数的格式
+            return number < 0 ? string.Format(culture, "-{0:0.#}", -number) : string.Format(culture, "{0:0.#}", number);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
