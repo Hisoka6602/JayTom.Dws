a9d3ade6883ef2bb98ab3b321bfbe5037e4f1ff0
Hisoka <770512100@qq.com>
2024-01-11 21:34:48 +0800

    20240111

---
+++ JayTom.Dws.LicenseApi/Controllers/LicenseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace JayTom.Dws.LicenseApi.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LicenseController : ControllerBase {
+    }
+}
