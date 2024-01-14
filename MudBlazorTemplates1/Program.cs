f2503c2a1a36fa03069a2853adc1da381a886a4e
Hisoka <770512100@qq.com>
2024-01-14 19:32:20 +0800

    20240114

---
+++ MudBlazorTemplates1/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using MudBlazorTemplates1;
+
+internal class Program {
+    private static async global::System.Threading.Tasks.Task Main(string[] args) {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddMudServices();
+
+        await builder.Build().RunAsync();
+    }
+}
