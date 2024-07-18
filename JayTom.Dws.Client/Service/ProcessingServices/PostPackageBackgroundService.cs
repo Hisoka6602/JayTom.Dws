959dd116b2593e36d8fa9dd85e8e1a100cee52af
Hisoka <770512100@qq.com>
2024-07-18 22:17:24 +0800

    20240718

--- JayTom.Dws.Client/Service/ProcessingServices/PostPackageBackgroundService.cs
+++ JayTom.Dws.Client/Service/ProcessingServices/PostPackageBackgroundService.cs
@@ -411,7 +411,7 @@ namespace JayTom.Dws.Client.Service.ProcessingServices {
                         var increaseCarCount = _grayscaleService.IncreaseCarCount((int)packageInfo.Guid,
                             _grayscaleDeviceSettingsDto.CarNumberOffset);
 
-                        var package = _grayscaleDeviceSettingsDto.CarNumberOffset == 0 ? info : PackageInfoManager.GetLastPackage(s => s.Value != null && s.Value.Guid.Equals(increaseCarCount));
+                        var package = _grayscaleDeviceSettingsDto.CarNumberOffset == 0 ? packageInfo : PackageInfoManager.GetLastPackage(s => s.Value != null && s.Value.Guid.Equals(increaseCarCount));
 
                         if (GrayScaleSkippedVehicles > 1) {
                             GrayScaleSkippedVehicles--;
@@ -495,6 +495,9 @@ namespace JayTom.Dws.Client.Service.ProcessingServices {
                         });
                     }
                     PackageInfoManager.AddPackage(packageInfo, packageRemoveTimers);
+
+                    //灰度仪操作放在这里
+
                     //触发创建包裹事件
                     EventAggregator.Instance.Publish(new TriggerPositionEvent() {
                         IsSuccess = true,
