7a7b4d34ad0d8684920c456fd9fc20f7e9a38772
Hisoka <770512100@qq.com>
2024-07-20 18:22:21 +0800

    20240720

--- JayTom.Dws.Client/ViewModels/Pages/Preferences/CameraConfiguration/NvrIpcDeviceManagementViewModel.cs
+++ JayTom.Dws.Client/ViewModels/Pages/Preferences/CameraConfiguration/NvrIpcDeviceManagementViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using JayTom.Dws.Client.Models.Cameras;
 
 namespace JayTom.Dws.Client.ViewModels.Pages.Preferences.CameraConfiguration {
 
@@ -11,5 +12,11 @@ namespace JayTom.Dws.Client.ViewModels.Pages.Preferences.CameraConfiguration {
     /// IPC/NVR管理
     /// </summary>
     public class NvrIpcDeviceManagementViewModel : BindableBase {
+        private IpcNvrItemInfoModel _ipcNvrItemInfo = new();
+
+        public IpcNvrItemInfoModel IpcNvrItemInfo {
+            get => _ipcNvrItemInfo;
+            set => SetProperty(ref _ipcNvrItemInfo, value);
+        }
     }
 }
