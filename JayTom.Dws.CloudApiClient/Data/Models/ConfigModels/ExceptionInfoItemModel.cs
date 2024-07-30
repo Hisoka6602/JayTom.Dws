3ac08dadf41a1c70c60e0aea2697230beab4daca
Hisoka <770512100@qq.com>
2024-07-30 20:28:47 +0800

    20240730

---
+++ JayTom.Dws.CloudApiClient/Data/Models/ConfigModels/ExceptionInfoItemModel.cs
@@ -0,0 +1,16 @@
+﻿namespace JayTom.Dws.CloudApiClient.Data.Models.ConfigModels {
+
+    public class ExceptionInfoItemModel {
+        public int Num { get; set; }
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 异常名称
+        /// </summary>
+        public string ExceptionName { get; set; } = string.Empty;
+        /// <summary>
+        /// 异常颜色
+        /// </summary>
+        public string ExceptionColor { get; set; } = string.Empty;
+    }
+}
---
+++ JayTom.Dws.CloudApiClient/Data/Models/ConfigModels/ExceptionMatchItemModel.cs
@@ -0,0 +1,62 @@
+﻿namespace JayTom.Dws.CloudApiClient.Data.Models.ConfigModels {
+
+    public class ExceptionMatchItemModel {
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int Num { get; set; }
+
+        /// <summary>
+        /// 异常匹配表唯一标识符
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 包含关键字
+        /// </summary>
+        public string Keywords { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 自定义正则表达式
+        /// </summary>
+        public string CustomRegex { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 数据源
+        /// </summary>
+        public DataSourceType DataSource { get; set; } = DataSourceType.ResponseContent;
+
+        /// <summary>
+        /// 异常类型名称
+        /// </summary>
+        public string ExceptionTypeName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 异常类型唯一标识符
+        /// </summary>
+        public long ExceptionTypeId { get; set; }
+        /// <summary>
+        /// 判断优先级
+        /// </summary>
+        public int Priority { get; set; }
+    }
+
+    public enum DataSourceType {
+
+        /// <summary>
+        /// 条码
+        /// </summary>
+        Barcode,
+
+        /// <summary>
+        ///提交内容
+        /// </summary>
+        RequestContent,
+
+        /// <summary>
+        /// 响应内容
+        /// </summary>
+        ResponseContent
+    }
+}
