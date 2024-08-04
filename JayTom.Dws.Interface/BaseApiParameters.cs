7d979ec2f177987b7f05920088df1ada9b17fc44
Hisoka <770512100@qq.com>
2024-08-04 15:09:44 +0800

    20240804

---
+++ JayTom.Dws.Interface/BaseApiParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace JayTom.Dws.Interface {
+
+    public class BaseApiParameters {
+
+        /// <summary>
+        /// Url
+        /// </summary>
+        public string Url { get; set; } = string.Empty;
+
+        /// <summary>
+        /// TimeOut
+        /// </summary>
+        public int TimeOut { get; set; }
+    }
+}
