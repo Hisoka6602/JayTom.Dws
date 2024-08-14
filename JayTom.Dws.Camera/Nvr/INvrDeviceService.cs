cb40c2c75251c42e65bba63abeacaa111a5516ef
Hisoka <770512100@qq.com>
2024-08-14 12:05:48 +0800

    20240814

---
+++ JayTom.Dws.Camera/Nvr/INvrDeviceService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace JayTom.Dws.Camera.Nvr {
+
+    /// <summary>
+    /// NVR设备管理
+    /// </summary>
+    public interface INvrDeviceService {
+    }
+
+    public class NvrDeviceInfo {
+    }
+}
